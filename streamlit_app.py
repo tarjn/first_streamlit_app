92a7f898351068e7ba90d947e8446f560c8291a9
tarjn <119733730+tarjn@users.noreply.github.com>
2022-12-03 16:31:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 streamlit.title('my parents healthy diner')
 streamlit.header('BreakFast Menu')
-streamlit.text('fish n chips')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
